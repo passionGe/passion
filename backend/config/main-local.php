8e4ca8de5c0600689734214d41362333198958c7
relievedge <relievedge@intv.com.cn>
2017-03-09 16:42:25 +0800

    yii frame

--- backend/config/main-local.php
+++ backend/config/main-local.php
@@ -1,9 +1,25 @@
 <?php
-return [
+
+$config = [
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
-            'cookieValidationKey' => 'GosN4DUoDQh5y0LzJao9GZGp7RF-ZoDS',
+            'cookieValidationKey' => '5JYSs_Db4FNWaUC-C1Z-GHRVftCLG0bF',
         ],
     ],
 ];
+
+if (!YII_ENV_TEST) {
+    // configuration adjustments for 'dev' environment
+    $config['bootstrap'][] = 'debug';
+    $config['modules']['debug'] = [
+        'class' => 'yii\debug\Module',
+    ];
+
+    $config['bootstrap'][] = 'gii';
+    $config['modules']['gii'] = [
+        'class' => 'yii\gii\Module',
+    ];
+}
+
+return $config;
--- frontend/config/main-local.php
+++ frontend/config/main-local.php
@@ -1,9 +1,25 @@
 <?php
-return [
+
+$config = [
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
-            'cookieValidationKey' => 'Hs5kOozF6etXRF7h5gyreQX-UBd_Ra--',
+            'cookieValidationKey' => 'HKINdoqxvNYfgMRu1qRVcRvSJWaPKVt-',
         ],
     ],
 ];
+
+if (!YII_ENV_TEST) {
+    // configuration adjustments for 'dev' environment
+    $config['bootstrap'][] = 'debug';
+    $config['modules']['debug'] = [
+        'class' => 'yii\debug\Module',
+    ];
+
+    $config['bootstrap'][] = 'gii';
+    $config['modules']['gii'] = [
+        'class' => 'yii\gii\Module',
+    ];
+}
+
+return $config;
