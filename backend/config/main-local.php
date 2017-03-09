9b483defd637c975cb1aaebb8559b2d5330358bb
relievedge <relievedge@intv.com.cn>
2017-03-09 16:02:57 +0800

    Yii framw

---
+++ backend/config/main-local.php
@@ -0,0 +1,9 @@
+<?php
+return [
+    'components' => [
+        'request' => [
+            // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
+            'cookieValidationKey' => 'GosN4DUoDQh5y0LzJao9GZGp7RF-ZoDS',
+        ],
+    ],
+];
